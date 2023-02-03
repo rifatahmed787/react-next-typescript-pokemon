abd34887972b5ae6a4cde206a2358b26f57e9b80
Rifat <mdrifatahmed787@gmail.com>
2023-02-03 16:38:34 +0600

    project initialized

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,12 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div>
+    <div className='text-center'>
+      <p>It is aowfull for me.</p>
+    </div>
+    <div>
+    <Component {...pageProps} />
+    </div>
+    </div>
 }
